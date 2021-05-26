66709479dc27d9d5a7d768859a4ab8bdf49a2417
Alex Vogt <alexvogt18@gmail.com>
2021-05-26 16:31:23 -0700

    added .data to book get request and adjusted render on bestbooks.js

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -18,7 +18,7 @@ class MyFavoriteBooks extends React.Component {
     await axios.get(`${process.env.REACT_APP_SERVER}/books`)
       .then(res => {
         this.setState({
-          bookData: res.books
+          bookData: res.data
         })
       })
   }
@@ -57,13 +57,18 @@ class MyFavoriteBooks extends React.Component {
         {this.state.bookData !== [] ?
           <Container>
             <Carousel>
-              <Carousel.Item>
+              {this.state.bookData.forEach((book) => {
+                <Carousel.Item>
                 <img className="d-block w-150"
                   src="https://via.placeholder.com/900x500/111111/111111?text=' '"
                   alt={`slide`} />
                 <Carousel.Caption>
+                  ${book.title}
+                  {console.log(book.title)}
                 </Carousel.Caption>
               </Carousel.Item>
+              })
+            }
             </Carousel>
           </Container> : null
         }
